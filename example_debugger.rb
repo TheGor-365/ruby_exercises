93e621689f2b19fcf188dde337846eb825f0af89
Gor <7495500@gmail.com>
2023-02-10 14:07:03 +0300

    Add debugger example app

---
+++ example_debugger.rb
@@ -0,0 +1,25 @@
+require 'debug'
+
+puts "Solve equetion: A * x^2 + B * x + C = 0"
+
+puts 'Enter A: '
+a = gets.to_f
+
+puts 'Enter B: '
+b = gets.to_f
+
+puts 'Enter C: '
+c = gets.to_f
+
+abort "It is linear equation" if a == 0
+
+discr = b * b - 4 * a * c
+
+binding.break
+x1 = (-b + Math::sqrt(discr)) / (2 * a)
+x2 = (-b - Math::sqrt(discr)) / (2 * a)
+
+puts "Solution 1:"
+puts x1
+puts "Solution 2:"
+puts x2
---
+++ exercise_movie_choice.rb
@@ -0,0 +1,14 @@
+require 'mechanize'
+
+agent = Mechanize.new
+chosen = false
+
+until chosen
+  page = agent.get("https://www.kinopoisk.ru/lists/movies/top500/#{rand(5)}/")
+  movie_block = page.search("div[styles_root__ti07r](@class, 'styles_root__ti07r')")
+  movie = page.search("//span[styles_position__TDe4E(@class, 'styles_position__TDe4E')]").to_a.sample
+  # movie_title
+  # movie_rating
+  # movie_link
+  # movie_overview
+end
